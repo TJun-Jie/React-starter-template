98335e28b5cec83a5815eb9d05ff065830d26d77
Jun Jie <62635032+TJun-Jie@users.noreply.github.com>
2023-01-14 17:47:24 +0800

    Auth

--- src/routing/PrivateRoute.tsx
+++ src/routing/PrivateRoute.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import {Route, Navigate} from "react-router-dom"
-import {useAuth} from "../AuthProvider";
+import {useAuth, useUserContext} from "../AuthProvider";
+import {Box, CircularProgress} from "@mui/material";
 
 const PrivateRoute = ({children}: any) => {
     let auth = useAuth();
 
+    if(auth.loading){
+        return (
+            <Box sx={{ marginTop: "30px"}}>
+               <CircularProgress />
+            </Box>
+
+            )
+    }
     if(!auth.user) {
         return <Navigate to="/login" />
     }
