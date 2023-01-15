a9fa1753fdba83eb8eb6b44673da6108bba82bf9
Jun Jie <62635032+TJun-Jie@users.noreply.github.com>
2023-01-15 11:06:53 +0800

    color

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from "react";
 import Button from '@mui/material/Button';
 
-import {Box, Typography, CircularProgress, Alert, Snackbar, LinearProgress} from "@mui/material";
-import { db } from "../config/.firebaseSetup"
-import { collection, addDoc, getDocs} from "firebase/firestore";
+import {Alert, Box, CircularProgress, LinearProgress, Snackbar, Typography} from "@mui/material";
+import {db} from "../config/.firebaseSetup"
+import {addDoc, collection, getDocs} from "firebase/firestore";
 import {TableModal} from "../components/TableModal";
 import {FaPlug} from "react-icons/fa";
 
@@ -33,12 +33,18 @@ export const MapPage = () => {
         getDocs(collection(db, "tables"))
             .then((querySnapshot) => {
                 const newData = querySnapshot.docs
-                    .map((doc) => ({...doc.data(), tableId : doc.id}));
-               let notAvailable = querySnapshot.docs
-               .map((doc) => ({...doc.data() as tableState}));
-               notAvailable = notAvailable.filter(table => !table.available);
-               setProgress((notAvailable.length/newData.length) * 100);
-               // @ts-ignore
+                    .map((doc) => (
+                        { ...doc.data(), tableId : doc.id }
+                    ));
+                let notAvailable = querySnapshot.docs
+                    .map((doc) => (
+                        { ...doc.data() as tableState }
+                    ));
+                notAvailable = notAvailable.filter(table => !table.available);
+                setProgress((
+                    notAvailable.length / newData.length
+                ) * 100);
+                // @ts-ignore
                 setTables(newData);
                 setIsLoading(false);
                 setRefreshData(false);
@@ -67,44 +73,57 @@ export const MapPage = () => {
     return (
         isLoading ?
             <Box sx={ { marginTop : "30px" } }>
-            <CircularProgress/>
+                <CircularProgress/>
             </Box>
             :
-        <Box sx={{ pt: 2}}>
-        <Typography variant="h5" >
-            Central Library Level 3
-        </Typography>
-        <Box sx={{mt:5, ml:10, mr:10}}>
-        <LinearProgress variant="determinate" value= {progress} sx={{height:30}}/>
-        </Box>
-        <Box display="flex"
-    justifyContent="center"
-    alignItems="center"
-    minHeight="80vh"
-    > 
-        <Box width={500} sx={{ padding: 10}}>
-            {
-                tables && tables.sort((table1, table2) => parseInt(table1.tableNumber) > parseInt(table2.tableNumber) ? 1 : -1).map(table => {
-                    return (
-                        <Button
-                            onClick={() => {
-                            setSelectedTable(table)
-                            setIsOpen(true)
-                        }}
-                            sx={{border: "1px solid black", margin: 1, ml: (parseInt(table.tableNumber) % 2 === 1) ? 1 : 10,  backgroundColor: table.available ? "#A2E4B8" : "#FF7276"}}
-                        startIcon={(table.plugs > 0) ? <FaPlug/> : ""}
-                        >{table.tableNumber}</Button>       
-                    )
-                })
-            }
-        </Box>
-        <TableModal isOpen={ isOpen } setIsOpen={ setIsOpen } selectedTable={ selectedTable } setRefreshData={setRefreshData} setSnackBarOpen={setSnackBarOpen}></TableModal>
-                <Snackbar open={snackBarOpen} autoHideDuration={6000} onClose={() => setSnackBarOpen(false)}>
-                    <Alert onClose={() => setSnackBarOpen(false)} severity="success" sx={{ width: '100%' }}>
-                        Complaint Submitted
-                    </Alert>
-                </Snackbar>
-    </Box>
-    </Box>
+            <Box sx={ { pt : 2 } }>
+                <Typography variant="h5">
+                    Central Library Level 3
+                </Typography>
+                <Box sx={ { mt : 5, marginLeft: "auto", marginRight:"auto",  width: "60%"} }>
+                    <Typography variant="h6" sx={{ marginBottom: "10px"}}>Crowd Level</Typography>
+                    <Box >
+                        <LinearProgress variant="determinate" value={ progress } sx={ { height : 30 } } color={progress < 75 ? "success" : "error"}/>
+                    </Box>
+                </Box>
+                <Box display="flex"
+                     justifyContent="center"
+                     alignItems="center"
+                     minHeight="80vh"
+                >
+                    <Box width={ 500 } sx={ { padding : 2 } }>
+                        {
+                            tables && tables.sort((table1, table2) => parseInt(table1.tableNumber) > parseInt(table2.tableNumber) ? 1 : -1).map(table => {
+                                return (
+                                    <Button
+                                        onClick={ () => {
+                                            setSelectedTable(table)
+                                            setIsOpen(true)
+                                        } }
+                                        sx={ {
+                                            border : "1px solid black",
+                                            margin : 1,
+                                            ml : (
+                                                parseInt(table.tableNumber) % 2 === 1
+                                            ) ? 1 : 10,
+                                            backgroundColor : table.available ? "#A2E4B8" : "#FF7276"
+                                        } }
+                                        startIcon={ (
+                                            table.plugs > 0
+                                        ) ? <FaPlug/> : "" }
+                                    >{ table.tableNumber }</Button>
+                                )
+                            })
+                        }
+                    </Box>
+                    <TableModal isOpen={ isOpen } setIsOpen={ setIsOpen } selectedTable={ selectedTable }
+                                setRefreshData={ setRefreshData } setSnackBarOpen={ setSnackBarOpen }></TableModal>
+                    <Snackbar open={ snackBarOpen } autoHideDuration={ 6000 } onClose={ () => setSnackBarOpen(false) }>
+                        <Alert onClose={ () => setSnackBarOpen(false) } severity="success" sx={ { width : '100%' } }>
+                            Complaint Submitted
+                        </Alert>
+                    </Snackbar>
+                </Box>
+            </Box>
     )
 }
