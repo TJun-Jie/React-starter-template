da06cedf8feec8be0e446d778ee3087640b0d58e
thamruicong <e0726692@u.nus.edu>
2023-01-15 12:16:09 +0800

    checkin checkout success pages mobile style

--- src/pages/CheckInOut/CheckInSuccess.tsx
+++ src/pages/CheckInOut/CheckInSuccess.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import {Alert, AlertTitle, Container} from "@mui/material";
+import {Alert, AlertTitle, Container, useTheme, useMediaQuery, Box} from "@mui/material";
 
 export const CheckInSuccess = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
     return (
-        <Container sx={{ width: "50%", marginLeft: "auto" , marginRight: "auto" , marginTop: 7}}>
-            <Alert severity="success" sx={{fontSize: "17px"}}>
+        <Container sx={{ width: isMobile ? "70%": "50%", marginLeft: "auto" , marginRight: "auto" , marginTop: 7}}>
+            <Alert severity="success" sx={{fontSize: "17px", alignItems:"center"}}>
+                <Box>
                 You have successfully <strong>checked in!</strong>
+                </Box>
             </Alert>
         </Container>
         )
--- src/pages/CheckInOut/CheckOutSuccess.tsx
+++ src/pages/CheckInOut/CheckOutSuccess.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import {Alert, AlertTitle, Box, Container} from "@mui/material";
+import {Alert, AlertTitle, Box, Container,  useTheme, useMediaQuery} from "@mui/material";
 import { useLocation } from "react-router-dom";
 import { Error } from "@mui/icons-material";
 
 export const CheckOutSuccess = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
      const location = useLocation();
         return (
 
-            <Container sx={{ width: "50%", marginLeft: "auto" , marginRight: "auto" , marginTop: 7}}>
-                    <Alert severity="success" sx={{fontSize: "17px"}}>
+            <Container sx={{ width: isMobile ? "70%": "50%", marginLeft: "auto" , marginRight: "auto" , marginTop: 7}}>
+                    <Alert severity="success" sx={{fontSize: "17px",  alignItems:"center"}}>
+                        <Box>
                         You have successfully <strong>checked out!</strong>
+                        </Box>
                     </Alert>
-                    {location.state.noiseComplaint > 0 ? <Alert severity="warning" sx={{mt:5}}>You have {location.state.noiseComplaint} noise complaint{location.state.noiseComplaint > 1 ? "s" : ""}, please be <strong>quieter</strong> next time. </Alert> : <div></div>}
+                    {location.state.noiseComplaint > 0 ? 
+                    <Alert severity="warning" sx={{mt:5, alignItems:"center"}}>
+                        <Box>
+                        You have {location.state.noiseComplaint} noise complaint{location.state.noiseComplaint > 1 ? "s" : ""}, please be <strong>quieter</strong> next time. 
+                        </Box>
+                        </Alert> : <div></div>}
             </Container>
         )
 }
